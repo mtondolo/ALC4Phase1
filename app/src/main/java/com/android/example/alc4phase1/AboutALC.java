c2ead1e0c31e351a9ee92f77d0845cb284249d5f
mtondolo <mtondolo@gmail.com>
2019-07-13 15:22:07 +0200

    Click on 'about andela' button

---
+++ app/src/main/java/com/android/example/alc4phase1/AboutALC.java
@@ -0,0 +1,13 @@
+package com.android.example.alc4phase1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutALC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_alc);
+    }
+}
--- app/src/main/java/com/android/example/alc4phase1/MainActivity.java
+++ app/src/main/java/com/android/example/alc4phase1/MainActivity.java
@@ -1,7 +1,10 @@
 package com.android.example.alc4phase1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the reference for About ALC Button
+        Button aboutALCButton = findViewById(R.id.tv_about_alc);
+
+        // Click on About ALC Button
+        aboutALCButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AboutALC.class);
+                startActivity(intent);
+            }
+        });
     }
 }
