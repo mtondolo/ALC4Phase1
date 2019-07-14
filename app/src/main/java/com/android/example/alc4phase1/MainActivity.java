1900b20c53ddf61b25bb540b721d8c0b405c6009
mtondolo <mtondolo@gmail.com>
2019-07-14 16:22:06 +0200

    Click on 'my profile' button

--- app/src/main/java/com/android/example/alc4phase1/MainActivity.java
+++ app/src/main/java/com/android/example/alc4phase1/MainActivity.java
@@ -14,8 +14,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Find the reference for About ALC Button
+        // Find the reference for About ALC and My Profile Buttons
         Button aboutALCButton = findViewById(R.id.tv_about_alc);
+        // Find the reference for About ALC Button
+        Button myProfileButton = findViewById(R.id.tv_my_profile);
 
         // Click on About ALC Button
         aboutALCButton.setOnClickListener(new View.OnClickListener() {
@@ -25,6 +27,15 @@ public class MainActivity extends AppCompatActivity {
                 // Url for our web page to be opened
                 String url = "https://andela.com/alc/";
                 openWebPage(url);
+            }
+        });
+
+        // Click on My Profile Button
+        myProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MyProfile.class);
+                startActivity(intent);
             }
         });
     }
